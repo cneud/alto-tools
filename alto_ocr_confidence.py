08b16aa86cf9cdb79b7ab380e01516aab253fbf9
WillemJan Faber <WillemJan.Faber@kb.nl>
2015-08-25 17:22:22 +0200

    Made pep8 compatible, also found a small bug

--- alto_ocr_confidence.py
+++ alto_ocr_confidence.py
@@ -5,20 +5,32 @@ import os
 import sys
 import xml.etree.cElementTree as ET
 
-namespace = {'alto-1': 'http://schema.ccs-gmbh.com/ALTO', 'alto-2': 'http://www.loc.gov/standards/alto/ns-v2#', 'alto-3': 'http://www.loc.gov/standards/alto/ns-v3#'}
+namespace = {'alto-1': 'http://schema.ccs-gmbh.com/ALTO',
+             'alto-2': 'http://www.loc.gov/standards/alto/ns-v2#',
+             'alto-3': 'http://www.loc.gov/standards/alto/ns-v3#'}
 
-for root, dirs, files in os.walk(sys.argv[1]):
-	for file in files:
-		if file.endswith('.xml') or file.endswith('.alto'):
-			fname = open(os.path.join(root, file))
-			for f in fname:
-				tree = ET.parse(fname)
-				score = 0
-				count = 0
-				for elem in tree.iterfind('.//alto-2:String', namespace): # make sure the correct namespace is set
-					wc = elem.attrib.get('WC')
-					score += float(wc)
-					count += 1
-				confidence = score/count
-				result = round(100 * (confidence), 2)
-				print('Filename: %s, Confidence: %s' % (file, result))
+
+def parse_alto(fh, filename):
+    tree = ET.parse(fh)
+    score = 0
+    count = 0
+    for elem in tree.iterfind('.//alto-2:String', namespace):
+            wc = elem.attrib.get('WC')
+            score += float(wc)
+            count += 1
+    confidence = score / count
+    result = round(100 * confidence, 2)
+    print('Filename: %s, Confidence: %s' % (filename, result))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python %s <inputdir>" % os.path.basename(__file__))
+        sys.exit(-1)
+
+    for root, dirs, files in os.walk(sys.argv[1]):
+        for filename in files:
+            if filename.endswith('.xml') or filename.endswith('.alto'):
+                fh = open(os.path.join(root, filename))
+                for f in fh:  # Dunno what to do with this?
+                    parse_alto(fh, filename)
+                fh.close()
