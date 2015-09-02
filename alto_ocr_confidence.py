13a1c1c5d0551f90fdb3aa07b8e90e9589103c53
cneud <clemens.neudecker@gmail.com>
2015-09-02 11:43:10 +0200

    replace print with sys.stdout.write

--- alto_ocr_confidence.py
+++ alto_ocr_confidence.py
@@ -6,35 +6,35 @@ import sys
 import xml.etree.cElementTree as ET
 
 namespace = {'alto-1': 'http://schema.ccs-gmbh.com/ALTO',
-             'alto-2': 'http://www.loc.gov/standards/alto/ns-v2#',
-             'alto-3': 'http://www.loc.gov/standards/alto/ns-v3#'}
+			 'alto-2': 'http://www.loc.gov/standards/alto/ns-v2#',
+			 'alto-3': 'http://www.loc.gov/standards/alto/ns-v3#'}
 
 def parse_alto(fh):
-    tree = ET.parse(fh)
-    score = 0
-    count = 0
-    xmlns = tree.getroot().tag.split('}')[0].strip('{') # extract namespace from root
-    if xmlns in namespace.values():
-        for elem in tree.iterfind('.//{%s}String' % xmlns):
-                wc = elem.attrib.get('WC')
-                score += float(wc)
-                count += 1
-        confidence = score / count
-        result = round(100 * confidence, 2)
-        print('File: %s, Confidence: %s' % (fh.name, result))
-    else:
-        print('ERROR: File "%s" does not appear to be a valid ALTO file (namespace declaration missing)' % fh.name)
+	tree = ET.parse(fh)
+	score = 0
+	count = 0
+	xmlns = tree.getroot().tag.split('}')[0].strip('{') # extract namespace from root
+	if xmlns in namespace.values():
+		for elem in tree.iterfind('.//{%s}String' % xmlns):
+				wc = elem.attrib.get('WC')
+				score += float(wc)
+				count += 1
+		confidence = score / count
+		result = round(100 * confidence, 2)
+		sys.stdout.write('\nFile: %s, Confidence: %s' % (fh.name, result))
+	else:
+		sys.stdout.write('\nERROR: File "%s" does not appear to be a valid ALTO file (namespace declaration missing)' % fh.name)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python %s <inputdir>" % os.path.basename(__file__))
-        sys.exit(-1)
+	if len(sys.argv) < 2:
+		print("Usage: python %s <inputdir>" % os.path.basename(__file__))
+		sys.exit(-1)
 
-    for root, dirs, files in os.walk(sys.argv[1]):
-        for filename in files:
-            if filename.endswith('.xml') or filename.endswith('.alto'):
-                fh = open(os.path.join(root, filename))
-                for f in fh: # loop over items in 'fh'
-                    parse_alto(fh)
-                fh.close()
+	for root, dirs, files in os.walk(sys.argv[1]):
+		for filename in files:
+			if filename.endswith('.xml') or filename.endswith('.alto'):
+				fh = open(os.path.join(root, filename))
+				for f in fh: # loop over items in 'fh'
+					parse_alto(fh)
+				fh.close()
