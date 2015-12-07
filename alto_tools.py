8bfcacde6df8e1d78005cbf10801ed03a7288570
cneud <clemens.neudecker@gmail.com>
2015-12-07 16:54:33 +0100

    making the mess worse...

--- alto_tools.py
+++ alto_tools.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
- 
+
 '''alto_tools.py: simple methods to perform operations on ALTO xml files'''
 
 import os
@@ -17,28 +17,28 @@ if len(scriptName) == 0:
 __version__= '0.0.1'
 
 
-def alto_parse(f):
+def alto_parse(alto):
     ''' Convert ALTO xml file to element tree '''
     try:
-        xml = ET.parse(f)
+        xml = ET.parse(alto)
     except ET.ParseError as e:
-        sys.stdout.write('\nERROR: Failed parsing "%s" - ' % fh.name + str(e))
+        sys.stdout.write('\nERROR: Failed parsing "%s" - ' % alto.name + str(e))
     #Register ALTO namespaces    
     namespace = {'alto-1': 'http://schema.ccs-gmbh.com/ALTO', 
                  'alto-2': 'http://www.loc.gov/standards/alto/ns-v2#',
                  'alto-3': 'http://www.loc.gov/standards/alto/ns-v3#'}
-                 # TODO: Resolve use of xsi/xlink
+                 # TODO: Handle xsi/xlink
     # Extract namespace from document root
     xmlns = xml.getroot().tag.split('}')[0].strip('{')
     if xmlns in namespace.values():
         return xml, xmlns
     else:
         sys.stdout.write('ERROR: File "%s" appears not to be a valid ALTO \
-            file (namespace declaration missing or not registered)' % fh.name)
+            file (namespace declaration missing or not registered)' % alto.name)
 
-def alto_text(xml, xmlns):
+def alto_text(alto):
     ''' Extract text content from ALTO xml file '''
-    xml, xmlns = alto_parse(fh)
+    xml, xmlns = alto_parse(alto)
     # Make sure to use UTF-8
     if sys.stdout.encoding != 'UTF-8':
         sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
@@ -52,9 +52,9 @@ def alto_text(xml, xmlns):
                 text = line.attrib.get('CONTENT') + ' '
                 sys.stdout.write(text)
 
-def alto_confidence(xml, xmlns):
-    ''' Calculate word confidence score for ALTO xml file '''
-    xml, xmlns = alto_parse(fh)
+def alto_confidence(alto):
+    ''' Calculate word confidence for ALTO xml file '''
+    xml, xmlns = alto_parse(alto)
     score = 0
     count = 0
     # Find all String elements
@@ -68,10 +68,12 @@ def alto_confidence(xml, xmlns):
         # Divide sum of WC values by number of words
         confidence = score / count
         result = round(100 * confidence, 2)
-        sys.stdout.write('\nFile: %s, Confidence: %s' % (fh.name, result))
+        sys.stdout.write('\nFile: %s, Confidence: %s' % (alto.name, result))
 
-def alto_transform(xml, xmlns, xsl):
+def alto_transform(alto, xsl):
     ''' Transform ALTO xml with XSLT '''
+    xml, xmlns = alto_parse(alto)
+    xsl = open('xsl', 'r', encoding='UTF8')
     # Detect if running on Windows
     if os.name == 'nt':
         # Check if msxsl.exe is present
@@ -96,9 +98,10 @@ def alto_transform(xml, xmlns, xsl):
     newdom = transform(dom)
     print(ET.tostring(newdom, pretty_print=True))
 
-def alto_metadata(xml, xmlns):
+def alto_metadata(alto):
     ''' Extract metadata from ALTO xml file '''
-    xml, xmlns = alto_parse(fh)
+    xml, xmlns = alto_parse(alto)
+    # Description
     sys.stdout.write('\n<Description>\n')
     try:
         xml.find('.//{%s}Description' % xmlns).find\
@@ -142,6 +145,7 @@ def alto_metadata(xml, xmlns):
     except AttributeError:
         sys.stdout.write(
             '\nMeasurementUnit            =   -- NOT_DEFINED --')
+    # OCRProcessing
     sys.stdout.write('\n\n<OCRProcessing>\n')
     try: 
         xml.find('.//{%s}OCRProcessing' % xmlns).text != None
@@ -150,6 +154,7 @@ def alto_metadata(xml, xmlns):
     except AttributeError:
         sys.stdout.write(
             '\nID                         =   -- NOT_DEFINED --')
+    # preProcessingStep
     sys.stdout.write('\n\n<preProcessingStep>\n')
     try: 
         xml.find('.//{%s}preProcessingStep' % xmlns).find\
@@ -231,6 +236,7 @@ def alto_metadata(xml, xmlns):
     except AttributeError:
         sys.stdout.write(
             '\napplicationDescription     =   -- NOT_DEFINED --')
+    # ocrProcessingStep
     sys.stdout.write('\n\n<ocrProcessingStep>\n')
     try: 
         xml.find('.//{%s}ocrProcessingStep' % xmlns).find\
@@ -311,7 +317,8 @@ def alto_metadata(xml, xmlns):
             ('{%s}applicationDescription' % xmlns).text)
     except AttributeError:
         sys.stdout.write(
-            '\napplicationDescription     =   -- NOT_DEFINED --') 
+            '\napplicationDescription     =   -- NOT_DEFINED --')
+    # postProcessingStep 
     sys.stdout.write('\n\n<postProcessingStep>\n')
     try: 
         xml.find('.//{%s}postProcessingStep' % xmlns).find\
@@ -395,25 +402,104 @@ def alto_metadata(xml, xmlns):
             '\napplicationDescription     =   -- NOT_DEFINED --')  
     sys.stdout.write('\n')
 
-# def alto_query():
-#     #TODO
+def alto_query(alto, query):
+    ''' Query ALTO xml file using XPath expressions '''
+    xml, xmlns = alto_parse(alto)
+    query = []
+    # TODO
 
-# def write_output():
-#     if len(output) == 0:
-#         sys.stdout.write()
-#     else:
-#         if args.text == True:
-#           output_filename = alto.name + '.txt'
-#           sys.stdout = open('output_filename', 'w')
-#         if args.metadata == True:
-#           output_filename = alto.name + '.md.txt'
-#           sys.stdout = open('output_filename', 'w')        
-#         if args.confidence == True:
-#           output_filename = alto.name + '.conf.txt'
-#           sys.stdout = open('output_filename', 'w')
-#         if args.transform == True:
-#           output_filename = alto.name
-#           sys.stdout = open('output_filename', 'w')
+#  Supported XPath syntax
+#  ----------------------
+#  Predicates (expressions within square brackets) must be preceded by a tag 
+#  name, an asterisk, or another predicate. 'position' predicates must be 
+#  preceded by a tag name.
+#  --------------------------------------------------------------------------
+# |        tag        |  Selects all child elements with the given tag.      |
+# |                   |  For example,'spam' selects all child elements named |
+# |                   |  'spam', and 'spam/egg' selects all grandchildren    |
+# |                   |  named 'egg' in all children named 'spam'.           |
+# |--------------------------------------------------------------------------|
+# |         *         |  Selects all child elements. For example, '*/egg'    |
+# |                   |  selects all children named 'egg'.                   |
+# |--------------------------------------------------------------------------|
+# |         .         |  Selects the current node. This is mostly useful at  |
+# |                   |  the beginning of the path, to indicate that it’s a  |
+# |                   |  relative path.                                      |
+# |--------------------------------------------------------------------------|
+# |         //        |  Selects all subelements, on all levels beneath the  |
+# |                   |  current element. For example, './/egg' selects all  |
+# |                   |  egg elements in the entire tree.                    |
+# |--------------------------------------------------------------------------|
+# |         ..        |  Selects the parent element. Returns 'None' if the   | 
+# |                   |  path attempts to reach the ancestors of the start   |
+# |                   |  element (the element 'find' was called on).         |
+# |--------------------------------------------------------------------------|
+# |     [@attrib]     |  Selects all elements that have the given attribute. |
+# |--------------------------------------------------------------------------|
+# | [@attrib='value'] |  Selects all elements for which the given attribute  |
+# |                   |  has the given value. The value cannot contain       |
+# |                   |  quotes.                                             |
+# |--------------------------------------------------------------------------|
+# |       [tag]       |  Selects all elements that have a child named 'tag'. |
+# |                   |  Only immediate children are supported.              |
+# |--------------------------------------------------------------------------|
+# |    [tag='text']   |  Selects all elements that have a child named 'tag'  | 
+# |                   |  whose complete text content, including descendants, |
+# |                   |  equals the given 'text'.                            |
+# |--------------------------------------------------------------------------|
+# |     [position]    |  Selects all elements that are located at the given  |
+# |                   |  position. The position can be either an integer (1  |
+# |                   |  (1 is the first position), the expression 'last()'  |
+# |                   |  (for the last position), or a position relative to  |
+# |                   |  the last position (e.g. 'last()-1').                |
+#  --------------------------------------------------------------------------
+
+def write_output():
+    ''' Write output to file(s) instead of stdout '''
+    if len(output) == 0:
+        sys.stdout.write()
+    else:
+        if args.text == True:
+            output_filename = alto.name + '.txt'
+            sys.stdout = open('output_filename', 'w')
+        if args.metadata == True:
+            output_filename = alto.name + '.md.txt'
+            sys.stdout = open('output_filename', 'w')        
+        if args.confidence == True:
+            output_filename = alto.name + '.conf.txt'
+            sys.stdout = open('output_filename', 'w')
+        if args.transform == True:
+            output_filename = alto.name
+            sys.stdout = open('output_filename', 'w')
+
+def web_app(alto):
+    ''' Simple webapp '''
+    import web
+    xml, xmlns = alto_parse(alto)
+    root = xml.getroot()
+    # Create a webserver to serve up the requests
+    urls = (
+        '/', 'index',
+        '/elements', 'list_elements',
+        '/attributes/(.*)', 'get_attributes')
+    app = web.application(urls, globals())
+    # Bind to localhost port 8888
+    app.run('0.0.0.0:8888')
+
+    class list_elements:        
+        def GET(self):
+            output = 'elements:[';
+            for child in root:
+                print('child', child.tag, child.attrib)
+                output += str(child.attrib) + ','
+                output += ']';
+                return output
+
+    class get_attributes:
+        def GET(self, elements):
+            for child in root:
+                if child.attrib['id'] == user:
+                    return str(child.attrib)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -448,13 +534,16 @@ def parse_arguments():
         dest='metadata',
         help='extract metadata of the ALTO document(s)')
     parser.add_argument('-x', '--transform',
-        action ='store_true',
-        default=False,
         dest='transform',
         help='transform ALTO document(s) to target format')
     parser.add_argument('-q', '--query',
         dest='query',
         help='query elements and attributes of the ALTO document(s)')
+    parser.add_argument('-w', '--web',
+        action='store_true',
+        default=False,
+        dest='web',
+        help='start webapp')
     args=parser.parse_args()
     return(args)
 
@@ -464,26 +553,27 @@ def main():
     for root, dirs, files in os.walk(sys.argv[1]):
         for filename in files:
             if filename.endswith('.xml') or filename.endswith('.alto'):
-                fh = open(os.path.join(root, filename), 'r', encoding='UTF8')
-                for alto in fh:
+                altofiles = open(os.path.join(root, filename), 'r', encoding='UTF8')
+                for alto in altofiles:
                     try:
                         alto_parse(alto)
                     except ET.ParseError as e:
                         sys.stdout.write('\nERROR: Failed parsing "%s" - ' % \
-                            fh.name + str(e))
-                        # Supported operations
+                            altofiles.name + str(e))
                         if args.confidence == True:
-                            alto_confidence()
+                            alto_confidence(alto)
                         elif args.text == True:
-                            alto_text()
+                            alto_text(alto)
                         elif args.metadata == True:
-                            alto_metadata()
+                            alto_metadata(alto)
                         elif args.transform == True:
-                            alto_transform
+                            alto_transform(alto, xsl)
                         elif args.query == True:
-                            alto_query
+                            alto_query(alto, query)
+                        elif args.web == True:
+                            web_app()
                         else:
-                            fh.close()
+                            altofiles.close()
                             sys.stdout.write('\nNo operation specified, aborting.')
 
 
