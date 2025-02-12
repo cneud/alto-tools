56f352bcccec81cac5d3dc5b9d1cdeb66e0f7078
Höper, Jakob <jakob.hoeper@sbb.spk-berlin.de>
2025-02-12 20:46:28 +0100

    restore input file encoding argument tests (#28)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import tempfile
-from typing import List
+from typing import Iterable, List
 
 import pytest
 
@@ -16,6 +16,37 @@ def argv(args: str) -> List[str]:
     ['alto-tools', '-c', 'file']
     """
     return ["alto-tools"] + args.split()
+
+
+@pytest.fixture
+def latin1_input_file_path() -> Iterable[str]:
+    with open("tests/data/PPN720183197-PHYS_0004.xml") as f:
+        xml = f.read()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        fn = os.path.join(tmpdir, "iso8859.xml")
+        with open(fn, "w+", encoding="iso-8859-1", errors="replace") as f:
+            f.write(xml)
+        yield fn
+
+
+def test_single_file_xml_encoding(
+    latin1_input_file_path: str,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
+    fn = latin1_input_file_path
+    sys.argv = argv(f"-t -x iso8859-1 {fn}")
+    alto_tools.main()
+    assert "Stille Gedanken" in capsys.readouterr().out
+
+
+def test_single_file_file_encoding(
+    latin1_input_file_path: str,
+    capsys: pytest.CaptureFixture[str],
+) -> None:
+    fn = latin1_input_file_path
+    sys.argv = argv(f"-t -e iso8859-1 {fn}")
+    alto_tools.main()
+    assert "Stille Gedanken" in capsys.readouterr().out
 
 
 def test_nonexistant_file_input(capsys: pytest.CaptureFixture[str]) -> None:
